39ba8cd28d858a1885e39f3d57aafdf62ba3ce51
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-04-28 23:26:58 +0530

    idk

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL : "https://shopping-app-136a2-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl.addEventListener("click", function()
-{
+addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    console.log(inputValue)
+    
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+
+    appendItemToShoppingListEl(inputValue)
 })
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
